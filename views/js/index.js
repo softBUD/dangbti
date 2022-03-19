7def60ab6a238fc9226d5ddc9eb3d57c797fce73
softBUD <chevimpala21@gmail.com>
2022-03-19 18:49:55 +0900

    0319 인트로 UI 전체수정

--- views/js/index.js
+++ views/js/index.js
@@ -1,31 +1,36 @@
 
 window.onload = function () {
-    let introBg = document.querySelector(".intro-container");
-    let title = document.querySelector(".titles");
-    let introbtn = document.querySelector(".intro-button");
-    let introbottom = document.querySelector(".intro-bottom");
-    let btnimage = document.querySelector(".button-img");
+    const introBg = document.querySelector(".intro-container");
+    const title = document.querySelector(".titles");
+    const introbtn = document.querySelector(".intro-button");
+    const btnimage = document.querySelector(".button-image")
+    const scrollbtn = document.getElementById("scroll-top");
 
+    scrollbtn.classList.add("show"); //scrolltop 요소 미표시
 
     window.addEventListener("scroll", function(event) {
-        let scroll = this.scrollY;
-
+        let scroll = window.pageYOffset
         introBg.style.transform = "translateY(" + scroll/3 + "px)";
         title.style.transform = "translateY(" + scroll/1.7 + "px)";
+        if(550 <= scroll) {
+          scrollbtn.classList.remove("show"); //scrolltop 요소 표시
+          } else {
+            scrollbtn.classList.add("show");
+          }
+        
     })
 
     introbtn.addEventListener("mouseover", function(event){
-        btnimage.src="../images/smiledog.png";
+      btnimage.src="../images/smiledog.png";
     })
 
     introbtn.addEventListener("mouseout",function(event) {
-        btnimage.src="../images/heartdog.png";
+      btnimage.src="../images/heartdog.png";
     })
 
-    setTimeout(function(){
-        document.querySelector('.intro-button-text').scrollIntoView({behavior:'smooth',block:"start"})
-    },2000);
-
+    scrollbtn.addEventListener("click", function(event){
+      window.scrollTo({top:0, behavior:"smooth"})
+    })
     //텍스트 모션
 
     let titlecon = document.querySelector(".title-container");
